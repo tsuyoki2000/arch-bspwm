0e5574a6c32b4ce2a2a9fcc3338514b396837e5e
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-07-11 11:20:19 +0900

    Update neovim-config-minimal.sh

--- script/neovim-config-minimal.sh
+++ script/neovim-config-minimal.sh
@@ -3,16 +3,19 @@
 ##########################
 # 最低限の Neovim の設定 #
 ##########################
+NVIM_CONFIG="${HOME}/.config/nvim/init.lua"
 
-#mkdir -p $HOME/.config/nvim/
-#touch $HOME/.config/nvim/init.lua
-echo "
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.clipboard = 'unnamedplus'
+if [ ! -e $NVIM_CONFIG ]; then
+  mkdir -p $HOME/.config/nvim/
+  #touch $HOME/.config/nvim/init.lua
+  echo "
+  vim.opt.number = true
+  vim.opt.relativenumber = true
+  vim.opt.clipboard = 'unnamedplus'
 
-vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
-vim.keymap.set('n', ';', ':')
-" >> $HOME/.config/nvim/init.lua
-
+  vim.g.mapleader = ' '
+  vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
+  vim.keymap.set('n', ';', ':')
+  " >> $NVIM_CONFIG
+  #$HOME/.config/nvim/init.lua
+fi
