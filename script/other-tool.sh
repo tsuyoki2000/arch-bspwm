836448e279a486347babd87f88f4da78084b6a5d
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-07-11 22:17:28 +0900

    Add files via upload

---
+++ script/other-tool.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+#
+##################
+# その他のツール #
+##################
+
+# インストールするパッケージ
+PACKAGE="
+firefox-i18n-ja
+arandr
+nitrogen
+picom
+polybar
+thunar
+"
+
+# パッケージのインストール
+sudo pacman -Syu $PACKAGE --needed --noconfirm
+
+# ~/.config/bspwm/bspwmrc の編集
+echo "
+# 解像度設定スクリプト
+$HOME/.screenlayout/display.sh
+
+# 背景画像
+nitrogen --restore
+
+# Polybar 起動スクリプト
+$HOME/.config/polybar/launch.sh
+
+# 透過処理
+picom &
+" >> $HOME/.config/bspwm/bspwmrc
+
