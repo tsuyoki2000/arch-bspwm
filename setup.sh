7cac44fdf46e41e3d8f43c7dd5dfa8e8b558878e
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-07-20 20:53:39 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,42 @@
 NORMAL=$(tput sgr0)
+RED=$(tput setaf 1)
 GREEN=$(tput setaf 2; tput bold)
 YELLOW=$(tput setaf 3)
-RED=$(tput setaf 1)
 
-echo "${GREEN}Install bspwm..."
+function red() {
+    echo -e "$RED$*$NORMAL"
+}
+
+function green() {
+    echo -e "$GREEN$*$NORMAL"
+}
+
+function yellow() {
+    echo -e "$YELLOW$*$NORMAL"
+}
+
+# bspwm ベースシステム
+green "Install bspwm..."
 bash script/bspwm.sh
+
+# fcitx5
+green "Install fcitx5..."
 bash script/fcitx5-system.sh
+
+# yay
+green "Install yay..."
 bash script/yay.sh
 
+# pacman-conf 編集
+green "Edit pacman-conf..."
 bash script/pacman-conf.sh
+
+# 最低限の Neovim 設定ファイルを作成
+green "Create neovim-config..."
 bash script/neovim-config-minimal.sh
+
+# その他のツールインストール
+green "Install Other Tool..."
 bash script/other-tool.sh
 
 # 再起動
