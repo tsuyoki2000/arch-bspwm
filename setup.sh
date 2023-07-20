05cdcd291e915cd20ec41a757ef4b4accca1ba4d
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-07-20 20:32:10 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-bash script/pacman-conf.sh
 bash script/bspwm.sh
 bash script/fcitx5-system.sh
 bash script/yay.sh
 
+bash script/pacman-conf.sh
 bash script/neovim-config-minimal.sh
 bash script/other-tool.sh
 
