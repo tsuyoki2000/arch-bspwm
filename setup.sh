e55e94f3454d59e74e806e2f980f15200a6d1b73
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-07-20 20:43:12 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+NORMAL=$(tput sgr0)
+GREEN=$(tput setaf 2; tput bold)
+YELLOW=$(tput setaf 3)
+RED=$(tput setaf 1)
+
+echo "${GREEN}Install bspwm..."
 bash script/bspwm.sh
 bash script/fcitx5-system.sh
 bash script/yay.sh
