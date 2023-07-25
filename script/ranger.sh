05584afdd8a685a3a33a630b684a2da2d8e881db
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-07-25 21:17:15 +0900

    Update ranger.sh

--- script/ranger.sh
+++ script/ranger.sh
@@ -1,1 +1,18 @@
+#! /bin/bash
 
+#########################################################
+# ranger を インストールするためのシェルスクリプト #
+#########################################################
+
+# パッケージのインストール
+# ranger（ranger本体）
+# highlight（ソースコードを色分け表示）
+# ueberzug（画像のプレビュー）
+sudo pacman -Syu ranger highlight ueberzug --needed --noconfirm
+
+# 設定ファイルの作成
+ranger --copy-config all
+
+# ~/.config/ranger/rc.conf の編集
+# 枠線の表示
+sed -i "s/set draw_borders none/set draw_borders both/g" $HOME/.config/ranger/rc.conf
