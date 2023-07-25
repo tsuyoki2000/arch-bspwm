a03eb29560d9118455856f1382fe89848759aad7
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-07-25 21:21:09 +0900

    Update ranger.sh

--- script/ranger.sh
+++ script/ranger.sh
@@ -16,3 +16,7 @@ ranger --copy-config all
 # ~/.config/ranger/rc.conf の編集
 # 枠線の表示
 sed -i "s/set draw_borders none/set draw_borders both/g" $HOME/.config/ranger/rc.conf
+# 画像表示を「true」にする
+sed -i "s/set preview_images false/set preview_images true/g" $HOME/.config/ranger/rc.conf
+# 画像の表示方法に「ueberzug」を指定
+sed -i "s/set preview_images_method w3m/set preview_images_method ueberzug/g" $HOME/.config/ranger/rc.conf
