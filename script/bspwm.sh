3d785290b0bd9d25d26675cf5727181cfbe59b78
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-08-02 23:54:03 +0900

    Update bspwm.sh

--- script/bspwm.sh
+++ script/bspwm.sh
@@ -31,6 +31,11 @@ cp /usr/share/doc/bspwm/examples/sxhkdrc $HOME/.config/sxhkd/
 #sed -i "s/urxvt/alacritty #urxvt/g" $HOME/.config/sxhkd/sxhkdrc
 sed -i "s/urxvt/xfce4-terminal #urxvt/g" $HOME/.config/sxhkd/sxhkdrc
 
+# /etc/environment にデフォルトアプリを設定
+ETC_ENV="/etc/environment"
+sudo sh -c "echo EDITOR=nvim >> $ETC_ENV"
+sudo sh -c "echo BROWSER=firefox >> $ETC_ENV"
+
 # ディスプレイ・マネージャー（sddm）のインストールとサービスの有効化
 sudo pacman -S sddm --noconfirm
 sudo systemctl enable sddm
