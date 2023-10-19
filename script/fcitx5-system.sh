bfaed81ee6c4d23394e7487516c450c0dce0850c
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-10-19 23:06:02 +0900

    Update fcitx5-system.sh

--- script/fcitx5-system.sh
+++ script/fcitx5-system.sh
@@ -16,11 +16,17 @@ sudo sh -c "echo GTK_IM_MODULE=fcitx >> $ETC_ENV"
 sudo sh -c "echo QT_IM_MODULE=fcitx >> $ETC_ENV"
 sudo sh -c "echo XMODIFIERS=@im=fcitx >> $ETC_ENV"
 
-# ログイン時に fcitx5 を自動起動するために /.config/bspwm/bspwmrc に追加
+# ログイン時に fcitx5 を自動起動するために ~/.config/bspwm/bspwmrc に追加
 echo "
 # fcitx5 自動起動
 fcitx5 &
 " >> $HOME/.config/bspwm/bspwmrc
+
+# インライン入力に対応するために、~/.config/fcitx5/conf/xim.conf を追加
+echo "
+# XIM で On The Spot スタイルを使う（再起動が必要）
+UseOnTheSpot=True
+" >> $HOME/.config/fcitx5/conf/xim.conf
 
 # /etc/environment を編集しているので、設定を有効にするために再起動が必要
 # （/etc/environment は OS起動時に読み込まれる）
