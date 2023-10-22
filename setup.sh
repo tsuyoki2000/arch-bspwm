72d449a1889afb771f3c5c2c5d77678445d5b3d8
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-10-22 23:14:56 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -79,7 +79,7 @@ green "Copying Config File..."
 # xfce4-terminal
 mkdir -p $HOME/.config/xfce4/xfconf/xfce-perchannel-xml
 cp .config/xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/
-cp usr/share/xfce4/terminal/colorschemes/alacritty.theme /usr/share/xfce4/terminal/colorschemes/
+sudo cp usr/share/xfce4/terminal/colorschemes/alacritty.theme /usr/share/xfce4/terminal/colorschemes/
 echo "done."
 
 # 再起動
