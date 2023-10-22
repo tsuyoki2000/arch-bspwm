47d7ba9121ebff59388ef35f0db66985ed364119
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-10-22 21:06:27 +0900

    Update setup.sh

--- .config/setup.sh
+++ .config/setup.sh
@@ -1,1 +1,7 @@
+# 設定ファイルのコピー
+green ""
+green "Copying Config File..."
+mkdir -p $HOME/.config/xfce4/xfconf/xfce-perchannel-xml
+cp config/xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml
+echo "done."
 echo "test"
