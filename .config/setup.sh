6d67a9adf5256af1e462af98e67a7f042c2f47a9
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-10-22 21:39:14 +0900

    Update setup.sh

--- .config/setup.sh
+++ .config/setup.sh
@@ -1,7 +1,1 @@
-#! /bin/bash
 
-##########################
-# 設定ファイルのコピー
-##########################
-mkdir -p $HOME/.config/xfce4/xfconf/xfce-perchannel-xml
-cp ./xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml
