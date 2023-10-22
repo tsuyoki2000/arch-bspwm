28d31faf5319e843af8c3d8731fe4de71dc9351c
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-10-22 17:53:55 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -72,6 +72,9 @@ green "Installing fish..."
 bash script/fish.sh
 
 # 設定ファイルのコピー
+green ""
+green "Copying Config File..."
+mkdir -p $HOME/.config/xfce4/xfconf/xfce-perchannel-xml
 cp config/xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml
 
 # 再起動
