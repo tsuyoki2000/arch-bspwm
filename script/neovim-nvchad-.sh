f9ba6d7678904cb172305525cbd17bb734b34343
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2024-05-04 22:12:42 +0900

    Update neovim-nvchad-.sh

--- script/neovim-nvchad-.sh
+++ script/neovim-nvchad-.sh
@@ -1,1 +1,15 @@
+#! /bin/bash
 
+##########################
+# NvChad（Neovimディストリビューション）のセットアップ #
+##########################
+NVIM_CONFIG="${HOME}/.config/nvim/init.lua"
+
+# ripgrep のインストール
+sudo pacman -S ripgrep
+
+if [ ! -e $NVIM_CONFIG ]; then
+  mkdir -p $HOME/.config/nvim/
+  git clone https://github.com/NvChad/starter $HOME/.config/nvim
+  rm -rf $HOME/.config/nvim/.git
+fi
