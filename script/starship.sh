b33edec491cbd82e7552f8a475b30bee64c3d7a6
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2024-05-04 10:41:46 +0900

    Update starship.sh

--- script/starship.sh
+++ script/starship.sh
@@ -7,12 +7,11 @@
 # パッケージのインストール
 sudo pacman -Sy starship --needed --noconfirm
 
-# ~/.config/fish/config.fish の編集
-echo "
-starship init fish | source" >> $HOME/.config/fish/config.fish
-
 # プリセットの適用（https://starship.rs/ja-JP/presets/）
 starship preset gruvbox-rainbow -o ~/.config/starship.toml
+
+# ~/.config/fish/config.fish の編集
+echo "starship init fish | source" >> $HOME/.config/fish/config.fish
 
 # ターミナルを再起動
 echo "Restart Terminal"
