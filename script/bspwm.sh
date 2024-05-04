f37f38e693691c5f7046864f2e3d85064493f792
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2024-05-04 14:54:08 +0900

    Update bspwm.sh

--- script/bspwm.sh
+++ script/bspwm.sh
@@ -46,7 +46,7 @@ alias vim='nvim'
 alias ll='ls -l'
 alias la='ls -lA'
 alias ..='cd ..'
-alias autoremove='sudo pacman -Rsc \$(pacman -Qdtq)'
+alias cleanup='sudo pacman -Rsc \$(pacman -Qdtq)'
 " >> $HOME/.bashrc
 
 # ~/.config/bspwm/bspwmrc の編集
