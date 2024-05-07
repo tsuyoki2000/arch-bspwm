4c470b45a724f10f30d7cc072b81006af03d1d66
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2024-05-07 17:01:30 +0900

    Create chaotic-aur.sh

---
+++ script/chaotic-aur.sh
@@ -0,0 +1,9 @@
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+# /etc/pacman.conf に情報追加
+PACMAN_CONF="/etc/pacman.conf"
+sudo sh -c "echo [chaotic-aur] >> $PACMAN_CONF"
+sudo sh -c "echo Include = /etc/pacman.d/chaotic-mirrorlist >> $PACMAN_CONF"
