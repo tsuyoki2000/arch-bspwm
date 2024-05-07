d34e67a32f2270d54e50fc1c8b6945eff31dd7b7
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2024-05-07 17:08:03 +0900

    Update chaotic-aur.sh

--- script/chaotic-aur.sh
+++ script/chaotic-aur.sh
@@ -1,3 +1,8 @@
+# AUR のコンパイル済みパッケージが置いてあるリポジトリ
+# AUR の全てのパッケージが置いてあるわけではない
+# このスクリプトは手動で実行する必要がある
+#（自動実行できるか試してない）
+
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
@@ -5,5 +10,6 @@ sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg
 
 # /etc/pacman.conf に情報追加
 PACMAN_CONF="/etc/pacman.conf"
+sudo sh -c "echo >> $PACMAN_CONF"
 sudo sh -c "echo [chaotic-aur] >> $PACMAN_CONF"
 sudo sh -c "echo Include = /etc/pacman.d/chaotic-mirrorlist >> $PACMAN_CONF"
